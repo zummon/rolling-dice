0961875e6dd0cfcbdb22d5c1fbb22e9abd675b3c
zummon <zummon.space@gmail.com>
2024-06-19 21:55:38 +0700

    up

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,40 @@
 <script>
-	import {onMount} from 'svelte'
-	let dices = ['⚀','⚁','⚂','⚃','⚄','⚅']
-	let randomNumbers = [0]
+  const dices = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
 
-	const randomize = () => {
-		randomNumbers = randomNumbers.map(() => {
-			return Math.floor(Math.random() * dices.length) 
-		})
-	}
-	onMount(() =>{
-		randomize()
-	})
+  let multiple = $state(2);
+  let ranNums = $derived.by(() => {
+    console.log("rolling");
+    return Array.from({ length: multiple }).map(() => {
+      return Math.floor(Math.random() * dices.length);
+    });
+  });
 </script>
 
-<div class="flex w-fit mx-auto mb-8 font-mono">
-	<button class="text-4xl px-3 py-2 text-[#b0725d] bg-white " on:click={() => {
-		randomize()
-	}}>Roll</button>
-	<input class="w-16 text-4xl text-center text-[#b0725d] bg-white " type="number" title="How many dices?" 
-	value={randomNumbers.length}
-	min="1"
-	max="8"
-		on:change={(e) => {
-		if (e.target.value >= 0) {
-			randomNumbers = Array.from({ length: e.target.value })
-			randomize()
-		}
-	}}>
+<div class="flex w-fit mx-auto py-8 font-mono">
+  <button
+    class="text-4xl px-3 py-2 text-[#b0725d] bg-white"
+    onclick={() => {
+      multiple++;
+      multiple--;
+    }}>Roll</button
+  >
+  <input
+    class="w-16 text-4xl text-center text-[#b0725d] bg-white"
+    type="number"
+    title="How many dices?"
+    bind:value={multiple}
+    min="1"
+    max="99"
+  />
 </div>
 
 <div class="bg-[#b0725d] container mx-auto">
-	<div class="grid grid-cols-2 mx-auto w-fit cursor-default">
-		{#each randomNumbers as randomNumber, index (index)}
-			<div class="text-8xl text-white p-4">{dices[randomNumber]}</div>
-		{/each}
-	</div>
-</div>
-
-<div class="text-center mt-8 text-white">
-	Made by zummon (Teerapat Anantarattanachai)<br>
-	Something breaks, shows incorrect result. Let me know.
+  <div class="flex flex-wrap gap-x-8 gap-y-4 justify-center cursor-default px-4 py-8">
+    {#each ranNums as ranNum, index (index)}
+      <div class="text-8xl">{dices[ranNum]}</div>
+    {/each}
+  </div>
+  <div class="text-center font-semibold">
+    Score {ranNums.reduce((prev, curr) => prev + curr + 1, 0)}
+  </div>
 </div>
