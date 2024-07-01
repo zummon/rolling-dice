8db668332f501aabbefa9328e8404f6c1b3072d1
zummon <zummon.space@gmail.com>
2024-07-01 19:31:56 +0700

    style

--- src/App.svelte
+++ src/App.svelte
@@ -10,16 +10,16 @@
   });
 </script>
 
-<div class="flex w-fit mx-auto py-8 font-mono">
+<div class="flex w-fit mx-auto pt-8 pb-1 font-mono">
   <button
-    class="text-4xl px-3 py-2 text-[#b0725d] bg-white"
+    class="text-4xl px-3 py-2 text-lime-950 bg-white"
     onclick={() => {
       multiple++;
       multiple--;
     }}>Roll</button
   >
   <input
-    class="w-16 text-4xl text-center text-[#b0725d] bg-white"
+    class="w-16 text-4xl text-center text-lime-950 bg-white"
     type="number"
     title="How many dices?"
     bind:value={multiple}
@@ -27,14 +27,12 @@
     max="99"
   />
 </div>
+<div class="text-center font-semibold pb-8">
+  Score {ranNums.reduce((prev, curr) => prev + curr + 1, 0)}
+</div>
 
-<div class="bg-[#b0725d] container mx-auto">
-  <div class="flex flex-wrap gap-x-8 gap-y-4 justify-center cursor-default px-4 py-8">
-    {#each ranNums as ranNum, index (index)}
-      <div class="text-8xl">{dices[ranNum]}</div>
-    {/each}
-  </div>
-  <div class="text-center font-semibold">
-    Score {ranNums.reduce((prev, curr) => prev + curr + 1, 0)}
-  </div>
+<div class="flex flex-wrap gap-x-8 gap-y-4 justify-center cursor-default px-4 py-8">
+  {#each ranNums as ranNum, index (index)}
+    <div class="text-8xl">{dices[ranNum]}</div>
+  {/each}
 </div>
