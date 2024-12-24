3f3059e4b94bf7571ee26e74bad442cd6f7fae3a
zummon <zummon.space@gmail.com>
2024-12-24 21:41:49 +0700

    save

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,47 @@
 <script>
-	import { onMount } from "svelte";
+	import { onMount } from "svelte"
 
-	let dices = $state(["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]);
-
+	let dices = $state(["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"])
 	let multiple = $state(2)
-	let ranNums = $state([])
 
-	function ranNumsFunc() {
-		console.log("rolling");
-		ranNums = Array.from({ length: multiple }).map(() => {
+	let rolled = $derived.by(() => {
+		let score = 0
+		let prepare = Array.from({ length: multiple }).map(() => {
+			let index = Math.floor(Math.random() * dices.length)
+			score += index + 1
 			return {
-				value: Math.floor(Math.random() * dices.length),
-				rotate: Math.floor(Math.random() * 180)
-			};
-		});
+				index: index,
+				rotate: Math.floor(Math.random() * 180),
+			}
+		})
+		return {
+			score: score,
+			dices: prepare,
+		}
+	})
+
+	function drop () {
+		multiple += 1
+		multiple += -1
 	}
 
 	onMount(() => {
-		ranNumsFunc();
+		
 	})
 </script>
 
-<div class="flex w-fit mx-auto pt-8 pb-1 font-mono">
-	<button class="text-4xl px-3 py-2 text-lime-950 bg-white" onclick={()=> { ranNumsFunc() }}>
+<div class="flex flex-wrap gap-4 items-center ">
+	<button class="px-3 font-semibold text-2xl border-neutral-500 border-b-2" title="Click to play" onclick={()=> { drop() }}>
 		Roll
 	</button>
-	<input class="w-16 text-4xl text-center text-lime-950 bg-white" type="number" title="How many dices?" min="1"
-		max="99" value={multiple} oninput={(e)=> { multiple = Number(e.target.value) ; ranNumsFunc() }} >
-</div>
-<div class="text-center font-semibold pb-8">
-	Score {ranNums.reduce((prev, curr) => prev + curr.value + 1, 0)}
+	<input class="w-16 text-center font-semibold text-2xl border-neutral-500 border-b-2 bg-transparent" type="number" title="How many dices?" min="1" max="99" bind:value={multiple} >
+	<div class="text-center text-2xl font-mono text-neutral-500">
+		Score {rolled.score}
+	</div>
 </div>
 
-<div class="flex flex-wrap gap-x-8 gap-y-4 justify-center cursor-default px-4 py-8">
-	{#each ranNums as ranNum}
-
-	<div class="text-9xl" style={`transform: rotate(${ranNum.rotate}deg)`}>{dices[ranNum.value]}</div>
+<div class="grow flex flex-wrap gap-x-8 gap-y-4 justify-center items-center cursor-default px-4 py-8 overflow-hidden">
+	{#each rolled.dices as dice}
+		<div class="text-9xl" style="transform: rotate({dice.rotate}deg)">{dices[dice.index]}</div>
 	{/each}
 </div>
